bce7ec02b2dad8faba60e617645a2de5cbd1a495
jxia1268 <from1268@gmail.com>
2017-11-15 22:15:32 +0900

    Update ThymeleafQuickReferenceApplication.java

--- ThymeleafQuickReference/ThymeleafQuickReferenceApplication.java
+++ ThymeleafQuickReference/ThymeleafQuickReferenceApplication.java
@@ -1,16 +1,1 @@
-package hoge;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-@SpringBootApplication
-@Controller
-public class ThymeleafQuickReferenceApplication extends WebMvcConfigurerAdapter {
-
-	public static void main(String[] args) throws Exception {
-		new SpringApplicationBuilder(Application.class).run(args);
-	}
-
-}
